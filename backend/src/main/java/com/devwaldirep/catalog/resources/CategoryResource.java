37d1614d914bb7daaf9eac5ae05de4e4a2383b35
wep1980 <wepbike@gmail.com>
2021-01-14 13:17:35 -0300

    Insert POST Category

--- backend/src/main/java/com/devwaldirep/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devwaldirep/catalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.devwaldirep.catalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devwaldirep.catalog.dto.CategoryDTO;
 import com.devwaldirep.catalog.service.CategoryService;
@@ -47,5 +51,60 @@ public class CategoryResource {
 		
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	
+	
+	/**
+	 * Os dados quando vem da tela, vem na forma de objeto ou seja ele recebe um CategoryDTO e o @RequestBody captura esse objeto que veio na requisição.
+	 * 
+	 * Quando e feita uma requisição e ela da certo, por padrão o codigo HTTP é 200(Requisição com sucesso), Mas no caso da criação de um novo recurso o 
+	 * correto e o codigo HTTP 201(Recurso criado).
+	 * 
+	 * Sera retornado no metodo o codigo 201 e um parametro adicional no cabeçalho da resposta que sera o endereço desse novo recurso criado.
+	 * @param dto
+	 * @return
+	 */
+	@PostMapping // No padrao REST quando e inserido um novo recurso e utilizado o padrão POST
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
