8d4164b69ef63855b5cfe919040e0df28546393f
wep1980 <wepbike@gmail.com>
2021-01-20 13:21:52 -0300

    Add front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; // TODO COMPONENTE REACT PRECISA TER ESSE IMPORT
+
+
+const App = () => {
+
+  return (
+      <h1>App</h1>
+  );
+  
+}
+
+
+export default App; // Exportando o componente App para que possa ser usado na aplicação
---
+++ front-web/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Importando o componente App
+import 'bootstrap/dist/css/bootstrap.css'; // Importe do CSS do BOOTSTRAP
+
+
+ReactDOM.render(
+ <App/>, 
+  document.getElementById('root') // Chamada da div root que fica em -> public/index.html
+);
+
+
