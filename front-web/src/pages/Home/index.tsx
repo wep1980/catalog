bfdd6ea3aa16f657655468a6cfe80ec755ec13cf
wep1980 <wepbike@gmail.com>
2021-01-21 14:59:08 -0300

    Page Home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import './styles.scss';
+import { ReactComponent as  MainImage } from '../../core/assets/images/main-image.svg'; // importe da imagem main(Principal) -- ReactComponent as  MainImage renomeando o nome do component
 
 
-// Como essa ArrowFunction e simples e vai retornar apenas um HTML
+/**
+ * Como essa ArrowFunction e simples e vai retornar apenas um HTML.
+ * row -> linha do Bootstrap.
+ * O BOOTASTRAP POR PADRÃO TRABALHA COM 12 COLUNAS, SE E COLOCADO COL-6 ESTAMOS TRABALHANDO COM METADO DO ESPEÇO
+ * col-6" -> 6 colunas, classe Bootstrap.
+ */
 const Home = () => (
-   <div>
-       <h1>Home</h1>
+   <div className="home-container">
+
+       <div className="row home-content">
+
+         <div className="col-6">
+            <h1 className="text-title">
+                Conheça o melhor <br/> catálogo de produtos
+            </h1>
+
+            <p className="text-subtitle">
+                Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
+            </p>
+         </div> 
+
+         <div className="col-6">
+            <MainImage className="main-image"/>
+         </div>
+
+       </div>
+
    </div>
 );
 
