082d843de75e5ce01210a4f86c67a866529d3d01
wep1980 <wepbike@gmail.com>
2021-02-01 10:17:07 -0300

    Finish product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
+import './styles.scss';
 
 
 // Como essa ArrowFunction e simples e vai retornar apenas um HTML
 const Catalog = () => (
-   <div>
-       <h1>Catalog</h1>
+   <div className="catalog-container">
+       <h1 className="catalog-title">
+           Catálogo de produtos
+       </h1>
+       <div className="catalog-products">
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+           <ProductCard/>
+       </div>
    </div>
 );
 
